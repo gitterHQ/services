80ddab315aa1deb3b429d38733231d8798182556
Andy Trevorah <andy@troupe.co>
2014-03-31 15:25:35 +0100

    sprintly is almost identical to old built in hook

--- test/sprintly.js
+++ test/sprintly.js
@@ -5,9 +5,14 @@ var examples = sprintly.examples;
 
 describe('Sprint.ly', function() {
 
-  it('should generate a success message', function() {
-    var body = examples['update'].body;
-    assert.equal(parse({}, body).message, 'Joe just created a new story: As a user, I want outbound service hooks so that I will see new items announced in Campfire or HipChat. (backlog)');
+  it('should generate a standard message', function() {
+    var payload = examples['update'];
+
+    var response = parse(payload.headers, payload.body);
+
+    assert.equal(response.message, 'Joe Stump created  \n[As a user, I want outbound service hooks so that I will see new items announced in Campfire or HipChat.](http://sprint.ly/i/1/116550/)');
+    assert.equal(response.icon, 'columns');
+    assert.equal(response.errorLevel, 'normal');
   });
 
 });
