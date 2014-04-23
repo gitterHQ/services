87a5f5c3bf328046d0b4ef0a139578e8238c8dc2
Andy Trevorah <andy@troupe.co>
2014-04-23 11:29:25 +0100

    fixed logentries playground

--- playground/server.js
+++ playground/server.js
@@ -50,7 +50,7 @@ var mapEventsList = function(events) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, X-Le-Account");
   next();
 });
 
